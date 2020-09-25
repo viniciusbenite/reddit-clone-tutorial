872e36a40a3354d336b2f76c526c28f4d5a6a480
Vinícius Ribeiro <viniciusribeiro@ua.pt>
2020-09-25 14:12:43 +0100

    Initial commit

---
+++ src/main/java/org/example/redditclone/model/Post.java
@@ -0,0 +1,55 @@
+package org.example.redditclone.model;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.lang.Nullable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @NotBlank(message = "Post title can't be empty")
+    private String postTitle;
+    
+    @Nullable
+    private String url;
+    
+    @Nullable
+    @Lob
+    private String description;
+    
+    @Builder.Default
+    private Integer voteCount = 0;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+    
+    private Instant createdDate;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id", referencedColumnName = "id")
+    private SubReddit subReddit;
+}
---
+++ src/main/java/org/example/redditclone/model/User.java
@@ -0,0 +1,41 @@
+package org.example.redditclone.model;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank(message = "Username is required")
+    private String username;
+    
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @Email
+    @NotEmpty(message = "Email is required")
+    private String email;
+    
+    private Instant createdOn;
+    private boolean enabled;
+}
